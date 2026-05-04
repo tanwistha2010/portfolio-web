010336e47b37e9f06f256481790ca93371103b6f
tanwistha2010 <tanwisthaguha39@gmail.com>
2026-05-04 14:20:04 +0530

    Added EducationServlet

---
+++ src/main/java/com/portfolio/servlet/EducationServlet.java
@@ -0,0 +1,24 @@
+package com.portfolio.servlet;
+
+import com.portfolio.util.PortfolioDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/education")
+public class EducationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            request.setAttribute("education", PortfolioDAO.getEducation());
+            request.getRequestDispatcher("education.jsp").forward(request, response);
+        } catch (SQLException e) {
+            request.setAttribute("error", "Unable to load education: " + e.getMessage());
+            request.getRequestDispatcher("education.jsp").forward(request, response);
+        }
+    }
+}
